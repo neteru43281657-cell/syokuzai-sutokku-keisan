e353823da4dd6577a99bb1ca9dc9f2c9d131bd24
neteru43281657-cell <neteru43281657@gmail.com>
2026-01-15 02:49:15 +0900

    Add files via upload

---
+++ data/fields.js
@@ -0,0 +1,12 @@
+const FIELDS = [
+  { id: "wakakusa", name: "ワカクサ本島", file: "ワカクサ本島.png.webp" },
+  { id: "cyan", name: "シアンの砂浜", file: "シアンの砂浜.png.webp" },
+  { id: "taupe", name: "トープ洞窟", file: "トープ洞窟.png.webp" },
+  { id: "snowfield", name: "ウノハナ雪原", file: "ウノハナ雪原.png.webp" },
+  { id: "lapis", name: "ラピスラズリ湖畔", file: "ラピスラズリ湖畔.png.webp" },
+  { id: "powerplant", name: "ゴールド旧発電所", file: "ゴールド旧発電所.png.webp" },
+  { id: "amber", name: "アンバー渓谷", file: "アンバー渓谷.png.webp" },
+  { id: "wakakusa_ex", name: "ワカクサ本島EX", file: "ワカクサ本島EX.png.webp" }
+]
+
+window.FIELDS = FIELDS;
---
+++ data/ingredients.js
@@ -0,0 +1,23 @@
+const INGREDIENTS = [
+  { id:"leek", name:"ふといながねぎ", file:"ふといながねぎ.webp" },
+  { id:"mushroom", name:"あじわいキノコ", file:"あじわいキノコ.webp" },
+  { id:"egg", name:"とくせんエッグ", file:"とくせんエッグ.webp" },
+  { id:"potato", name:"ほっこりポテト", file:"ほっこりポテト.webp" },
+  { id:"apple", name:"とくせんリンゴ", file:"とくせんリンゴ.webp" },
+  { id:"herb", name:"げきからハーブ", file:"げきからハーブ.webp" },
+  { id:"soymeat", name:"マメミート", file:"マメミート.webp" },
+  { id:"milk", name:"モーモーミルク", file:"モーモーミルク.webp" },
+  { id:"honey", name:"あまいミツ", file:"あまいミツ.webp" },
+  { id:"oil", name:"ピュアなオイル", file:"ピュアなオイル.webp" },
+  { id:"ginger", name:"あったかジンジャー", file:"あったかジンジャー.webp" },
+  { id:"tomato", name:"あんみんトマト", file:"あんみんトマト.webp" },
+  { id:"cacao", name:"リラックスカカオ", file:"リラックスカカオ.webp" },
+  { id:"tail", name:"おいしいシッポ", file:"おいしいシッポ.webp" },
+  { id:"soy", name:"ワカクサ大豆", file:"ワカクサ大豆.webp" },
+  { id:"corn", name:"ワカクサコーン", file:"ワカクサコーン.webp" },
+  { id:"coffee", name:"めざましコーヒー", file:"めざましコーヒー.webp" },
+  { id:"pumpkin", name:"ずっしりカボチャ", file:"ずっしりカボチャ.webp" },
+  { id:"avocado", name:"つやつやアボカド", file:"つやつやアボカド.webp" }
+];
+
+window.INGREDIENTS = INGREDIENTS;
